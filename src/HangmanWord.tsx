7f2f123b865c4c2047a9c01348122a62f2211e5f
John Sison <132543192+jls777@users.noreply.github.com>
2025-09-23 03:51:32 -0400

    hangmanwords

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function HangmanWord() {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetters = ["t", "g", "e"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
