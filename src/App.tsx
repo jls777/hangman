5c464b1da46e9c43d450190a8c485b397e08fd88
John Sison <132543192+jls777@users.noreply.github.com>
2025-09-23 03:05:34 -0400

    added hangmandrawing hangmanword keyboard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -9,7 +12,30 @@ function App() {
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   console.log(wordToGuess);
-  return <h1>hi</h1>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HangmanDrawing() {
+  return <div>HangmanDrawing</div>;
+}
