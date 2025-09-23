2dd42661cd19412dcfd16447a3064d6d2a608074
John Sison <132543192+jls777@users.noreply.github.com>
2025-09-23 02:56:40 -0400

    added usestates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import words from "./wordList.json";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test");
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
   return <h1>hi</h1>;
 }
 
